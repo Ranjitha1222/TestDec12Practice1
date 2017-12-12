e3b4052d2300612de8850096b0e19eedeea0a7d6
Ranjitha1222 <34330662+Ranjitha1222@users.noreply.github.com>
2017-12-12 12:11:40 +0530

    Second Changes

--- GitBuildDemo/src/com/demo/HelloWorld.java
+++ GitBuildDemo/src/com/demo/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
     System.out.println("This is a Jenkins build Demo1 2");
+    System.out.println("This is the line modifying");
 	}
 
 }
